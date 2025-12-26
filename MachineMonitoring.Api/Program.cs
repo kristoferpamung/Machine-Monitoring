ae7141f1fa206a8451b052d270bc21213f0f839f
Kristofer Bayu <kristoferpamung@gmail.com>
2025-12-26 15:08:14 +0700

    edit swagger configuration

--- MachineMonitoring.Api/Program.cs
+++ MachineMonitoring.Api/Program.cs
@@ -2,6 +2,7 @@ using MachineMonitoring.Api.Data;
 using MachineMonitoring.Api.Repositories;
 using MachineMonitoring.Api.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Machine Monitoring API",
+        Version = "v1",
+        Description = "API for real-time machine production monitoring",
+        Contact = new OpenApiContact
+        {
+            Name = "Kristofer"
+        }
+    });
+} );
 
 builder.Services.AddDbContext<MachineMonitoringDbContext>(options =>
     options.UseSqlite("Data Source=MachineMonitoring.db"));
